d09a8b68a88a6bb9961ecf7c69557014f5fe3a5e
Davavalo <86305205+Davavalo@users.noreply.github.com>
2022-05-13 16:17:38 -0500

    simplifying scripts
    need to add styles now

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const rock = 0;
 const paper = 1;
 const scissors = 2;
 const rockPaperScissors = [rock, paper, scissors];
-let playerSelection;
-
+let playerScore = 0
+let computerScore = 0
 
 function computerPlay() {
     let choice = Math.floor(Math.random() * 3);
@@ -14,30 +14,65 @@ function playerOptions(selection){
     if (typeof selection === 'string'){
         selection = selection.toLowerCase();
         if (selection === "rock"){
-            playerSelection = 0;
+            return 0;
         } else if (selection === "paper"){
-            playerSelection = 1;
+            return 1;
         } else if (selection === "scissors"){
-            playerSelection = 2;
+            return 2;
         }
+    }
 }
 
-
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 0 && computerSelection === 1){
-        console.log("You Lost!");
-    } else if (playerSelection === 0 && computerSelection === 2){
-        console.log("You Won!");
-    } else if (playerSelection === 1 && computerSelection === 0){
-        console.log("You Won");
-    } else if (playerSelection === 1 && computerSelection === 2){
-        console.log("You Lost!");
-    } else if (playerSelection === 2 && computerSelection === 0){
-        console.log("You Lost");
-    } else if (playerSelection === 2 && computerSelection === 1){
-        console.log("You Win!");
-    } else if (playerSelection === computerSelection){
-        console.log("It's a Tie");
-    }
+    if (playerSelection === computerSelection){
+        alert("It's a Tie");
+    } else if (
+        (playerSelection === 0 && computerSelection === 1) || 
+        (playerSelection === 1 && computerSelection === 2) ||
+        (playerSelection === 2 && computerSelection === 0)
+        ){ 
+            computerScore ++;
+            alert("You Lost");
+    } else if (
+        (playerSelection === 0 && computerSelection === 2) ||
+        (playerSelection === 1 && computerSelection === 0) ||
+        (playerSelection === 2 && computerSelection === 1)
+        ){
+            playerScore++;
+            alert("You Won");
+    }    
   } 
 
+  function game(){
+    let enterChoice = prompt("Rock, Paper, or Scissors?")
+    enterChoice = enterChoice.toLowerCase()
+    if (
+        (enterChoice === "rock") ||
+        (enterChoice === "paper") ||
+        (enterChoice === "scissors")
+            ){
+                playRound(playerOptions(enterChoice), computerPlay())
+                checkScore()
+    } else {
+        alert("That's Not A Valid Choice")
+        game()
+    }
+  }
+
+  function resetGame() {
+      playerScore = 0
+      computerScore = 0
+  }
+
+  function checkScore(){
+    if (computerScore === 5){
+        alert(`You Lost, Final Score = Player ${playerScore}, Computer ${computerScore}`)
+        resetGame()
+    } else if (playerScore === 5) {
+        alert(`You Won, Final Score = Player ${playerScore}, Computer ${computerScore}`)
+        resetGame()
+    } else if (playerScore != 5 || computerScore !=5){
+        game()
+    }
+  }
+  
